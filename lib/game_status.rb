01a22ec5e556def2138c10bb17279941e6325643
Ian Gutwinski <iangutwinski@gmail.com>
2019-01-21 23:21:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      combo
+      puts combo
     else
       false
     end
